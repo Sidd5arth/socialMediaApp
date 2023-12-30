d47f409ece8fc48ad6503b79408a888cef4fd9b8
Siddharth Singh Kushwaha <siddharthmnbv@gmail.com>
2023-12-30 11:28:57 +0530

    coverage

--- src/Tests/Navbar.test.tsx
+++ src/Tests/Navbar.test.tsx
@@ -1,16 +1,18 @@
 import React from "react";
-import { render, screen } from "@testing-library/react";
+import { render, screen, act } from "@testing-library/react";
 import "@testing-library/jest-dom";
 import { MemoryRouter } from "react-router-dom";
 import Navbar from "../Components/Navbar";
 
 describe("Navbar component", () => {
   test("renders Navbar component with basic navigation items", () => {
-    render(
-      <MemoryRouter>
-        <Navbar />
-      </MemoryRouter>
-    );
+    act(() => {
+      render(
+        <MemoryRouter>
+          <Navbar />
+        </MemoryRouter>
+      );
+    });
 
     expect(screen.getByText("Home")).toBeInTheDocument();
     expect(screen.getByText("Likes")).toBeInTheDocument();
