3966977a85366e5003182f7a1158aa2a1c333451
Siddharth Singh Kushwaha <siddharthmnbv@gmail.com>
2024-04-21 23:58:10 +0530

    removed files

--- src/hooks/useMistral.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-type Props = {};
-
-const useMistral = (props: Props) => {
-  return <div>useMistral</div>;
-};
-
-export default useMistral;
--- src/hooks/useSDXL.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-type Props = {};
-
-const useSDXL = (props: Props) => {
-  return <div>useSDXL</div>;
-};
-
-export default useSDXL;
